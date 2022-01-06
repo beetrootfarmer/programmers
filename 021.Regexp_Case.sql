df708fd2f9a3d8b3fbc033204e7038baf28ab900
beetrootfarmer <87971876+beetrootfarmer@users.noreply.github.com>
2022-01-06 21:49:04 +0900

    REGEXP case

---
+++ 021.Regexp_Case.sql
@@ -0,0 +1,19 @@
+-- REGEXP를 활용한 CASE문으로 사용해서 보호소 동물의 중성화 여부 파악
+-- SELECT절에서 사용되는 정규표현식!
+--  => WHERE SEX_UPON_INTAKE REGEXP "(Neutered|Spayed)"
+SELECT ANIMAL_ID,
+        NAME,
+        CASE WHEN SEX_UPON_INTAKE LIKE "%Neutered%" OR SEX_UPON_INTAKE LIKE "%Spayed%"
+             THEN "O" 
+             ELSE "X" END AS "중성화" 
+FROM ANIMAL_INS
+ORDER BY 
+    ANIMAL_ID ASC;
+
+
+    SELECT ANIMAL_ID,
+       NAME,
+       CASE WHEN SEX_UPON_INTAKE LIKE '%Neutered%' OR SEX_UPON_INTAKE LIKE '%Spayed%'
+            THEN 'O'
+            ELSE 'X' END AS '중성화'
+FROM ANIMAL_INS
