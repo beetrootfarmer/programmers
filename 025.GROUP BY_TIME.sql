a60cbf8a51795502911e5f443483b53f44d3043d
beetrootfarmer <87971876+beetrootfarmer@users.noreply.github.com>
2022-01-10 16:07:51 +0900

    group by

--- 025.GROUP BY_TIME.sql
+++ 025.GROUP BY_TIME.sql
@@ -1,12 +1,15 @@
 -- 시간별 보호소 입양 카운트
+--  몇시 대에 가장 입양이 활발한지 시간대별 입양 건수 조회하기 
 -- COUNT, GROUPBY
 -- COUNT(DATETIME)
 -- WHERE DATETIME %h = "(9-19)""
 -- GROUP BY DATETIME()
-SELECT COUNT(DATETIME)
+SELECT HOUR(DATETIME) as HOUR, COUNT(DATETIME) AS COUNT
 FROM ANIMAL_INS
-WHERE DATETIME %h = "(9-19)"
-GROUP BY DATETIME(COUNT)
+-- WHERE DATETIME %h = "(9-19)"
+WHERE HOUR(DATETIME) between 9 and 19
+GROUP BY HOUR(DATETIME)
+ORDER BY 1;
 
 SELECT hour(datetime) as HOUR, count(datetime) as COUNT
 from animal_outs
